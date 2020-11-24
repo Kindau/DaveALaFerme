5505d653bb2e305391595652563448c935179ca6
Melisson Verstraete <melisson.verstraete@gmail.com>
2020-11-24 17:06:56 +0100

    Essai Mel

--- pDaveALaFerme/main.cpp
+++ pDaveALaFerme/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 int main()
 {
+    //Je suis un Comm
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
